d6129ae1b7b9985370a46669f9a613d60bbf60ec
Eirik Sletteberg <eiriksletteberg@gmail.com>
2013-09-03 13:26:16 +0200

    Use new Finagle API

--- recipe-thrift-server/src/main/java/javaexample/RecipeThriftServer.java
+++ recipe-thrift-server/src/main/java/javaexample/RecipeThriftServer.java
@@ -2,18 +2,18 @@ package javaexample;
 
 import java.net.InetSocketAddress;
 
+import com.twitter.finagle.ListeningServer;
 import com.twitter.finagle.Thrift;
+import com.twitter.util.Await;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import javaexample.RecipeService.Service;
 
 public final class RecipeThriftServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         RecipeThriftServerHandler handler = new RecipeThriftServerHandler();
-        Service service = new Service(handler, new TBinaryProtocol.Factory());
+        RecipeService.Service service = new RecipeService.Service(handler, new TBinaryProtocol.Factory());
         InetSocketAddress listen = new InetSocketAddress(1234);
-        Thrift.serve(listen, service);
-        while(true){
-
-        }
+        ListeningServer server = Thrift.serve(listen, service);
+        Await.result(server);
     }
 }
--- recipe-thrift-web/src/main/java/javaexample/RecipeWebServer.java
+++ recipe-thrift-web/src/main/java/javaexample/RecipeWebServer.java
@@ -2,25 +2,16 @@ package javaexample;
 
 import java.net.InetSocketAddress;
 
+import com.twitter.finagle.HttpServer;
+import com.twitter.finagle.ListeningServer;
 import com.twitter.finagle.builder.ServerBuilder;
+import com.twitter.util.Await;
 
 public final class RecipeWebServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         RecipeWebServerHandler handler = new RecipeWebServerHandler();
         InetSocketAddress listen = new InetSocketAddress(5678);
-
-        /*
         ListeningServer server = HttpServer.serve(listen, handler);
-        while(true){
-
-        }
-        */
-
-        ServerBuilder.safeBuild(handler, ServerBuilder.get()
-                .codec(com.twitter.finagle.http.Http.get())
-                .name("HttpServer")
-                .bindTo(listen));
-
-
+        Await.result(server);
     }
 }
