b6f19aee6cbd904f2f1b6b092a915dbe5190ea83
Eirik Sletteberg <eiriksletteberg@gmail.com>
2013-09-03 13:30:07 +0200

    Skeleton for Future example

---
+++ recipe-thrift-web/src/main/java/javaexample/FutureTransformSkeleton.java
@@ -0,0 +1,11 @@
+package javaexample;
+
+import com.twitter.util.Await;
+import com.twitter.util.Future;
+import com.twitter.util.FutureTransformer;
+
+public class FutureTransformSkeleton {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
